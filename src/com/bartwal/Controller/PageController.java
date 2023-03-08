7653a39f8790470ef016cc6f9164f8fe4f868ac0
GaganBartwal <sports.singh1998@gmail.com>
2023-03-08 18:58:20 +0530

    Commit Test 3

---
+++ src/com/bartwal/Controller/PageController.java
@@ -0,0 +1,26 @@
+package com.bartwal.Controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PageController extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        dispatcher.forward(request, response);
+
+    }
+
+    public void LoginSuccessfullPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("HomePage.jsp");
+        dispatcher.forward(request, response);
+
+    }
+
+}
